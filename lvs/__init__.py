88036731d7f2dc0ce4b614973a47cffbe50783f1
Joe Ryan <jr@aphyt.com>
2023-11-28 09:01:39 -0800

    Add read latest records functionality

--- lvs/__init__.py
+++ lvs/__init__.py
@@ -7,24 +7,27 @@ import pyodbc
 
 
 class LVSDispatcher:
-    def __init__(self):
+    def __init__(self, upper_read_index: int = 0):
         self.connection = None
         self.cursor = None
+        self.upper_read_index = upper_read_index
+        self.conn_string = ''
 
     def connect(self, host_server: str, user: str, password: str, database: str, port: int = 3306):
-        conn_string = (
+        self.conn_string = (
             f"DRIVER={{MariaDB ODBC 3.1 Driver}};"
             f"User={user};"
             f"Password={password};"
             f"Database={database};"
             f"Server={host_server};"
-            f"Port={str(port)};")
-        self.connection = pyodbc.connect(conn_string)
+            f"Port={str(port)}")
+        self.connection = pyodbc.connect(self.conn_string)
         self.cursor = self.connection.cursor()
 
     def record_count(self):
         try:
-            self.cursor.execute(f'select count(*) from ReportData where Sequence=0;')
+            self.connection.commit()
+            self.cursor.execute(f'select count(*) from ReportData where Sequence=0')
             rows = self.cursor.fetchone()
             return rows[0]
         except pyodbc.ProgrammingError as error:
@@ -47,21 +50,25 @@ class LVSDispatcher:
             self.cursor.execute(f'select * from ReportData where '
                                 f'ReportID={str(index)}')
             record = self.cursor.fetchall()
-            if record is None:
-                return record
-            else:
-                return record
+            self.upper_read_index = index
+            return record
         except pyodbc.ProgrammingError as error:
             print(f'Reports not accessible: {error}')
 
-    def get_previous_n_records(self, number_of_records: int):
-        record_count = self.record_count()
+    def get_previous_n_records(self, number_of_records: int, record_count: int = None):
+        if record_count is None:
+            record_count = self.record_count()
         record_read_index = record_count - number_of_records + 1
         record_list = []
         for index in range(number_of_records):
             record_list.append(self.get_record(record_read_index))
             record_read_index += 1
         return record_list
+
+    def read_upper_records(self):
+        current_count = self.record_count()
+        number_of_records = current_count - self.upper_read_index
+        return self.get_previous_n_records(number_of_records, current_count)
 
     def __del__(self):
         if self.cursor is not None:
