661f0ba76443eeef1c8aeb0a6b6e36364666eb31
Joe Ryan <jr@aphyt.com>
2023-11-28 12:46:16 -0800

    Update example_lvs_actions.py

--- example_lvs_actions.py
+++ example_lvs_actions.py
@@ -10,23 +10,13 @@ if __name__ == '__main__':
     lvs_instance = lvs.LVSDispatcher()
     lvs_instance.connect('192.168.250.115', 'LVS-95XX', 'LVS-95XX', 'LVS-95XX')
     count = lvs_instance.record_count()
-    # print(count)
-    # for i in range(count):
-    #     text = lvs_instance.symbol_text(i+1)
-    #     if text is not None:
-    #         print(text)
-    #
+    print(count)
+    for i in range(count):
+        text = lvs_instance.symbol_text(i+1)
+        if text is not None:
+            print(text)
+
     records = lvs_instance.get_previous_n_records(5)
     for record in records:
         print(record)
-
-    lvs_instance.upper_read_index = count
-    try:
-        while True:
-            records = lvs_instance.read_upper_records()
-            for record in records:
-                print(record)
-            time.sleep(1.0)
-    except KeyboardInterrupt:
-        pass
 
